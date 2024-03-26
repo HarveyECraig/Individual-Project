70594ccdf6a2d8aa7baffaf1e232ae5c39779b2e
HarveyECraig <142818450+HarveyECraig@users.noreply.github.com>
2024-03-25 23:02:02 -0600

    Update main.java

--- src/main.java
+++ src/main.java
@@ -14,6 +14,8 @@ void setup() {
   permx = width/2;
   permy = height/2;
   size(900, 800);
+  size(900, 800);
+  player = new Player(width / 2, height / 2, 0.1); // Initialize the player
   
   
 }
@@ -21,45 +23,50 @@ void draw() {
   background(white);
   stroke(0);
   //rect(0, 500, 900, 300);
-  if (isPressed == true) {
-    planet(mouseX, mouseY);
-  } else {
-    planet(permx, permy);
-  }
+  //if (isPressed == true) {
+  //  planet(mouseX, mouseY);
+  //} else {
+  //  planet(permx, permy);
+  //}
+  
+  player.update();
+  player.checkEdges();
+  player.interact();
+  player.display();
  
 }
+// Commented all this out as it is no longer in use for now (Transfered it to its own class)
+//void planet(float x, float y) {
 
-void planet(float x, float y) {
 
-
-  ellipse(x, y, 30, 30);
-  fill(green);
-   y = y + speed;
-  speed = speed + gravity;
-  if (mousePressed == true && mouseX < x+50 && mouseX > x-50 && mouseY < y + 50 && mouseY > y -50) {
-    isPressed = true;
-  } else {
-    isPressed = false;
-    permx = x;
-    permy = y;
-  }
- if (x-15 <= 0 || x+15 >= width) {
-    fill(red);
-  } else {
-    fill(green);
-  }
-  if (y-15 <= 0 || y+15 >= height) {
-    fill(red);
-  }
-  if (y > height) {
-    speed = speed * -0.95;
-  //  if (abs(speed) < 3){
-  //    speed = 0;
-  //  }
-    y = height;
-  }
-  println("speed:" + speed);
-}
+//  ellipse(x, y, 30, 30);
+//  fill(green);
+//   y = y + speed;
+//  speed = speed + gravity;
+//  if (mousePressed == true && mouseX < x+50 && mouseX > x-50 && mouseY < y + 50 && mouseY > y -50) {
+//    isPressed = true;
+//  } else {
+//    isPressed = false;
+//    permx = x;
+//    permy = y;
+//  }
+// if (x-15 <= 0 || x+15 >= width) {
+//    fill(red);
+//  } else {
+//    fill(green);
+//  }
+//  if (y-15 <= 0 || y+15 >= height) {
+//    fill(red);
+//  }
+//  if (y > height) {
+//    speed = speed * -0.95;
+//  //  if (abs(speed) < 3){
+//  //    speed = 0;
+//  //  }
+//    y = height;
+//  }
+ // println("speed:" + speed);
+//}
 void floor(int h, int y){
   rect(0, y, 900, h);
 }
