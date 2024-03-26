e59a1e270e31fac5d3de2405be75ec380b6fa253
HarveyECraig <142818450+HarveyECraig@users.noreply.github.com>
2024-03-26 12:34:02 -0600

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,7 @@ class Player {
   float x, y; // Position of the player
   float speed; // Vertical speed of the player
   float gravity; // Gravity affecting the player
+  float xvel;
 
   // Constructor
   Player(float x, float y, float gravity) {
@@ -16,6 +17,16 @@ class Player {
   void update() {
     y += speed;
     speed += gravity;
+    x += xvel;
+    xvel = 0;
+      if(key == 'd'){
+    xvel = 5;
+  } else if(key == 'a'){
+    xvel = -5;
+    key = 0;
+ 
+  println(key);
+}
   }
 
   // Function to display the player
@@ -34,16 +45,19 @@ class Player {
     if (mousePressed && isMouseNear()) {
       speed = 0;
     }
+    
   }
-
+//void keyReleased(){
+//  key = 0;
+//}
   // Function to handle bouncing
   void checkEdges() {
     if (x - 15 <= 0 || x + 15 >= width || y - 15 <= 0 || y + 15 >= height) {
-      speed *= -0.95;
+      speed *= -0.8;
     }
     if (y > height) {
       y = height;
-      speed *= -0.95;
+      speed *= -0.8;
     }
   }
 }
