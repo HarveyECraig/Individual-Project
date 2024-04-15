09ff8a8d5359f74017b2d667a2a07b201a09d4f2
HarveyECraig <142818450+HarveyECraig@users.noreply.github.com>
2024-04-15 14:49:04 -0600

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -3,17 +3,15 @@ class Player {
   float speed; // Vertical speed of the player
   float gravity; // Gravity affecting the player
   float xvel;
-  boolean isJumping;
-  float jumpStrength;
+  boolean jump = false;
 
 
   // Constructor
-  Player(float x, float y, float gravity, float jumpStrength) {
+  Player(float x, float y, float gravity) {
     this.x = x;
     this.y = y;
     speed = 0;
     this.gravity = gravity;
-    this.jumpStrength = jumpStrength;
   }
 
   // Function to update the position of the player
@@ -22,49 +20,63 @@ class Player {
     speed += gravity;
     x += xvel;
     xvel = 0;
-  
-    if (isJumping){
-      speed -= jumpStrength;
-      isJumping = false;
-    }
-    
     if (key == 'd') {
       xvel = 5;
     } else if (key == 'a') {
       xvel = -5;
-    } else if (key == 'w' && !isJumping) {     
-      isJumping = true;
+    } else if (key == 'w'){
+      for (int i = -1; i < 0; i = i+1){
+         speed -= 0.5;
+      }
     }
 
     if (keyPressed == false) {
-      isJumping = false;
+
       xvel = 0;
     }
-//if (speed < 0.4 && speed > -0.4) {
-//    speed = 0;
-//    y = 800;
-//  }
-    //System.out.println(xvel);
+
+    System.out.println(speed);
     System.out.println(key);
   }
 
 
   // Function to display the player
   void display() {
-    ellipse(x, y, 30, 30);
     fill(green);
+    ellipse(x, y, 30, 30);
   }
+  // Need to change isMouseNear & Interact to use WASD instead of mouse
+  // Function to check if the mouse is near the player
+  boolean isMouseNear() {
+    return mouseX < x + 50 && mouseX > x - 50 && mouseY < y + 50 && mouseY > y - 50;
+  }
+
+  // Function to handle interactions with the player
+  void interact() {
+    if (mousePressed && isMouseNear()) {
+      speed = 0;
+    }
+  }
+
   // Function to handle bouncing
   void checkEdges() {
-    if (x - 15 <= 0 || x + 15 >= width || y - 15 <= 0 || y + 15 >= height) {
+    if (x - 15 <= 0 || x + 15 >= width || y - 15 <= 0 || y + 15 >= height || y + 15>= floor1.y) {
       speed *= -0.8;
     }
     if (y > height) {
+     if(speed > 0){
       y = height;
+      speed *= -0.8;
+     } else if (speed < 0){
+       y = height;
+       speed -= 10;
+     }
     }
-    
-    System.out.println(speed);
   }
+  //boolean intersect (Floor f){
+  //  float d = dist(x, y, f.x, f.y);
+  //  if
+  //}
 }
 
 
