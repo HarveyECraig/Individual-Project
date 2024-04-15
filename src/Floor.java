518bc88889db9d5952368fa9ce7821f556dc5d38
HarveyECraig <142818450+HarveyECraig@users.noreply.github.com>
2024-04-15 14:49:26 -0600

    Update Floor.java

--- src/Floor.java
+++ src/Floor.java
@@ -1,15 +1,18 @@
-class Floor{
-  int x = 300, y = 300, h = 50;
-  
-  Floor(int x, int y, int h){
+class Floor {
+  int x, y, h;
+
+  Floor(int x, int y, int h) {
     this.x = x;
     this.y = y;
     this.h = h;
-    
   }
-  void display(){
+  void display() {
     fill(75);
-     rect(x, y, 110, h);
+    stroke(100);
+    rect(x, y, 900, h);
   }
-  Floor floor;
+//  int findPos {
+  
+//}
 }
+Floor floor;
